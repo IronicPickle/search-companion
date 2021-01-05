4040ed5f838183c2ad941960acf53e7421b2e57f
Admin3 <nathanrath@live.co.uk>
2021-01-05 14:40:18 +0000

    Groundwork

--- src/react/popup.tsx
+++ src/react/popup.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
 
-export default function Popup() {
-  useEffect(() => {
-    // Example of how to send a message to eventPage.ts.
-    chrome.runtime.sendMessage({ popupMounted: true });
-  }, []);
 
-  return <div className="popupContainer">Hello, world!</div>;
+class Popup extends React.Component {
+  render() {
+    return <div>Hello, world!</div>
+  }
 }
+
+ReactDOM.render(<Popup/>, document.getElementById("popupContainer"))
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  devtool: false,
   entry: {
     background: path.join(__dirname, "src/scripts/background.ts"),
     popupPage: path.join(__dirname, "src/react/popup.tsx")

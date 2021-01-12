1b7883058d7684952867e536987af1ca3747bae0
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 01:36:33 +0000

    Added comments

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   devtool: false,
   entry: {
-    "js/background/main": path.join(__dirname, "src/background/main.ts"),
+    "js/background/main": path.join(__dirname, "src/background/main.ts"), // Handles interface script injection
 
-    "js/content/interfaces/cms": path.join(__dirname, "src/content/interfaces/cms.ts"),
-    "js/content/interfaces/terra": path.join(__dirname, "src/content/interfaces/terra.ts"),
-    "js/content/interfaces/simpleSearch": path.join(__dirname, "src/content/interfaces/simpleSearch.ts"),
+    "js/content/interfaces/cms": path.join(__dirname, "src/content/interfaces/cms.ts"), // CMS interface
+    "js/content/interfaces/terra": path.join(__dirname, "src/content/interfaces/terra.ts"), // TerraFirma interface
+    "js/content/interfaces/simpleSearch": path.join(__dirname, "src/content/interfaces/simpleSearch.ts"), // Simple Search interface
 
-    "js/react/popup": path.join(__dirname, "src/react/popup.tsx"),
-    "js/react/embed": path.join(__dirname, "src/react/embed.tsx")
+    "js/react/popup": path.join(__dirname, "src/react/popup.tsx"), // Popup react root
+    "js/react/embed": path.join(__dirname, "src/react/embed.tsx") // Embed react root
   },
   module: {
     rules: [

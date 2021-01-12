013c7b051d0c5eb5ec0f8754c19f7c65c2530bed
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 01:34:33 +0000

    Removed unused shared theme options

--- src/react/themes.ts
+++ src/react/themes.ts
@@ -1,15 +1,7 @@
 import { createMuiTheme, Theme, ThemeOptions } from "@material-ui/core";
 
 const shared:ThemeOptions = {
-  zIndex: {
-    mobileStepper: 2000000,
-    speedDial: 3000000,
-    appBar: 4000000,
-    drawer: 5000000,
-    modal: 6000000,
-    snackbar: 7000000,
-    tooltip: 8000000
-  }
+
 }
 
 export const lightTheme: Theme = createMuiTheme({
